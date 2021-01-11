428b7174881e4c5a665217b9cf466462adec33b6
jeesusmies <46649783+jeesusmies@users.noreply.github.com>
2021-01-11 21:23:23 +0200

    make illustration

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ ILLUSTRATION;
+ 
+var bot = new Bot(">") // prefix
+
+[bot.Event]
+public async Task OnUserJoin(ctx) {
+  Console.WriteLine(ctx.User.name);
+}
+
+[bot.Command]
+[Alias(["p", "pi"])]
+public async Task Ping(ctx) { // also usable by command name
+  ctx.Send("Pong!");
+}
+*/
