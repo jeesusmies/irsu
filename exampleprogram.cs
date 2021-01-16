0fff31ac233584aef19b87dd9879c6644f71f7c5
jeesusmies <46649783+jeesusmies@users.noreply.github.com>
2021-01-16 15:23:37 +0200

    Create exampleprogram.cs

---
+++ exampleprogram.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using umaru.Library.Attributes.Command;
+using umaru.Library;
+
+class TestClass
+{
+    public static void Main(string[] args)
+    {
+        var client = new Client("jeesusmies", "password", "");
+        client.Start();
+    }
+
+    [Event]
+    public static async Task OnReady(Context ctx)
+    {
+        Console.WriteLine("Bot is ready!");
+    }
+
+    [Event]
+    public static async Task OnMessage(Context ctx)
+    {
+        Console.WriteLine($"<{ctx.Message.Author}> -> " +
+                          $"{ctx.Message.Channel}: " +
+                          $"{ctx.Message.Contents}");
+                          // output example; <jeesusmies> -> #osu: fuck pp!
+    }
+
+    [Command]
+    [Alias(new [] { "ping" })] // command can be called using Ping or ping
+    public static async Task Ping(Context ctx) 
+    {
+        await ctx.Send("Pong!");
+    }
+}
